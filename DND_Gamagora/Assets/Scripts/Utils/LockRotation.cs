aa38b4fe916676851a7da64e1ef8ec39f4a559b2
Maël Gonzalez <mael.gnzlz@gmail.com>
2016-01-31 20:38:55 +0100

    Add walkable meteor

---
+++ DND_Gamagora/Assets/Scripts/Utils/LockRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockRotation : MonoBehaviour {
+
+    public bool local;
+
+	void Update () {
+        if (local)
+            transform.localRotation = Quaternion.identity;
+        else
+            transform.rotation = Quaternion.identity;
+    }
+    
+}
